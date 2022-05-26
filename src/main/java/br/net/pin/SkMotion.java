75f6b5b7cf6b62668479e822ac80ec91e58bb130
Éverton M. Vieira <emuvi@outlook.com.br>
2022-05-25 21:01:46 -0300

    make a pool of captured to be save on a parallel thread

--- src/main/java/br/net/pin/SkMotion.java
+++ src/main/java/br/net/pin/SkMotion.java
@@ -4,9 +4,9 @@ import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
-import java.io.File;
-
-import javax.imageio.ImageIO;
+import java.util.Deque;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class SkMotion {
   private static boolean changed(BufferedImage img1, BufferedImage img2) {
@@ -23,18 +23,47 @@ public class SkMotion {
     return false;
   }
 
-  private static void save(BufferedImage image) throws Exception {
-    ImageIO.write(image, "jpg", new File("screen.jpg"));
-  }
+  private static Integer count = 0;
+
+  private static Deque<BufferedImage> deque = new ConcurrentLinkedDeque<>();
+
+  public static final Long startTime = System.currentTimeMillis();
+
+  private static final AtomicBoolean isRunning = new AtomicBoolean(true);
 
   public static void main(String[] args) throws Exception {
+    var saving = new Thread() {
+      public void run() {
+        while (true) {
+          var image = deque.pollLast();
+          if (image != null) {
+            count++;
+            // try {
+            // ImageIO.write(image, "jpg", new File("screen" + count + ".jpg"));
+            // } catch (IOException e) {
+            // e.printStackTrace();
+            // }
+          } else if (!isRunning.get()) {
+            break;
+          }
+        }
+      };
+    };
+    saving.start();
     Robot robot = new Robot();
     Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
-    BufferedImage img1 = robot.createScreenCapture(screenRect);
-    Thread.sleep(3000);
-    BufferedImage img2 = robot.createScreenCapture(screenRect);
-    if (changed(img1, img2)) {
-      save(img2);
+    BufferedImage previous = robot.createScreenCapture(screenRect);
+    var index = 0;
+    while (index < 500) {
+      BufferedImage current = robot.createScreenCapture(screenRect);
+      if (changed(previous, current)) {
+        deque.addFirst(current);
+      }
+      previous = current;
+      index++;
     }
+    isRunning.set(false);
+    System.out.println("Time: " + (System.currentTimeMillis() - startTime));
+    saving.join();
   }
 }
