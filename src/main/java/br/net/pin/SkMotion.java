4885b33ab8929535fbea06be413e125248b38dd7
Éverton M. Vieira <emuvi@outlook.com.br>
2022-05-26 20:16:58 -0300

    removes radiance

--- src/main/java/br/net/pin/SkMotion.java
+++ src/main/java/br/net/pin/SkMotion.java
@@ -4,14 +4,12 @@ import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
-import org.pushingpixels.radiance.theming.api.skin.RadianceGraphiteSunsetLookAndFeel;
-
 public class SkMotion {
   public static void main(String[] args) throws Exception {
     SwingUtilities.invokeLater(() -> {
       try {
         JFrame.setDefaultLookAndFeelDecorated(true);
-        UIManager.setLookAndFeel(new RadianceGraphiteSunsetLookAndFeel());
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         new Interface().show();
       } catch (Exception e) {
         e.printStackTrace();
