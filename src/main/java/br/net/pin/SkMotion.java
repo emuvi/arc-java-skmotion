a70954cd51a1db99de78d6078c0a607d6bf32ad7
Éverton M. Vieira <emuvi@outlook.com.br>
2022-05-27 11:27:01 -0300

    apply FlatDarculaLaf on interface swing look and feel

--- src/main/java/br/net/pin/SkMotion.java
+++ src/main/java/br/net/pin/SkMotion.java
@@ -1,15 +1,15 @@
 package br.net.pin;
 
-import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
+
+import com.formdev.flatlaf.FlatDarculaLaf;
 
 public class SkMotion {
   public static void main(String[] args) throws Exception {
     SwingUtilities.invokeLater(() -> {
       try {
-        JFrame.setDefaultLookAndFeelDecorated(true);
-        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        UIManager.setLookAndFeel(new FlatDarculaLaf());
         new Interface().show();
       } catch (Exception e) {
         e.printStackTrace();
